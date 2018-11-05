8e99247491e1bbb3060224061523d06cfc5aa5e2
Ashish Bhatia <ashishb@ashishb.net>
2018-11-04 22:09:38 -0800

    Add release setup scripts

---
+++ release/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+import sys, os
+
+_DIR_OF_THIS_SCRIPT = os.path.split(__file__)[0]
+_VERSION_FILE_NAME = 'version.txt'
+_VERSION_FILE_PATH = os.path.join(_DIR_OF_THIS_SCRIPT, 'androide', _VERSION_FILE_NAME)
+_README_FILE_NAME = 'README.md'
+_README_FILE_PATH =  os.path.join(_DIR_OF_THIS_SCRIPT, _README_FILE_NAME)
+
+with open(_VERSION_FILE_PATH, 'r') as fh:
+    version = fh.read().strip()
+
+with open(_README_FILE_PATH, 'r') as fh:
+    long_description = fh.read()
+
+setup(name='android-sdk-enhanced',
+        version=version,
+        description='A better version of the command-line android SDK manager tool with a more intuitive interface.',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        classifiers=['Intended Audience :: Developers'], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        keywords='Android ADB developer',
+        author='Ashish Bhatia',
+        author_email='ashishb@ashishb.net',
+        url='https://github.com/ashishb/android-sdk-enhanced',
+        license='Apache',
+        packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+        include_package_data=True,
+        zip_safe=True,
+        install_requires=[
+                # -*- Extra requirements: -*-
+                'docopt',
+                'psutil',
+                ],
+        entry_points={
+                # -*- Entry points: -*-
+                'console_scripts': [
+                        'androide=androide.main:main',
+                        ],
+                }
+        )
